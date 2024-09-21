155ad2603c14420c562045bbf114438bcd9bf783
VannessChaw <vannesscjk-wp21@student.tarc.edu.my>
2024-09-21 23:09:49 +0800

    Update home.php

--- View/home.php
+++ View/home.php
@@ -15,10 +15,6 @@
             }
             
             if(isset($_SESSION['currentUserModel']) && $_SESSION['currentUserModel']['role']['roleID'] == 2){
-                echo "Welcome, " . htmlspecialchars($_SESSION['currentUserModel']['full_name']) . "!";
-                //testing
-                echo '<a href="CustomerTicketPage.php" class="link-box">Book Ticket</a>';
-                
                 
             } elseif(isset($_SESSION['currentUserModel']) && $_SESSION['currentUserModel']['role']['roleID'] == 1) {
                 header("Location: index.php?controller=admin&action=displayAdminMainPanel");
