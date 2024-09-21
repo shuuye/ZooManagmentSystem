251b7f7395d683600b0efb66a66c8276cd6e314b
diedInside-fml <chewws-wp21@student.tarc.edu.my>
2024-09-21 21:22:28 +0800

    Update CustomerTicketPage.php

--- CustomerTicketPage.php
+++ CustomerTicketPage.php
@@ -29,7 +29,6 @@
     <div class="topnav">
         <a href="index.php"><img src="assests/Logo-Zoo-Negara.png" alt="Zoo Negara Home Page" width="60px" height="50px"/></a>
         <a href="index.php?controller=user&action=showMembership">Membership</a><!--set thing you gonna navigate for customer here-->
-        <a href="#contact">Contact</a>
 
         <?php
         if (session_status() === PHP_SESSION_NONE) {
