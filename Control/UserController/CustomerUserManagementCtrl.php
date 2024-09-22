a605d0802a1d476a9e0494621671177e40b5fa67
diedInside-fml <chewws-wp21@student.tarc.edu.my>
2024-09-22 15:00:27 +0800

    Update CustomerUserManagementCtrl.php

--- Control/UserController/CustomerUserManagementCtrl.php
+++ Control/UserController/CustomerUserManagementCtrl.php
@@ -1,59 +1,5 @@
-<<<<<<< HEAD
 <?php
-
-    require_once __DIR__ . '/../../Config/webConfig.php';
-    $webConfig = new webConfig();
-    $webConfig->restrictAccessForNonLoggedInAdmin();
-
-    require_once 'UserController.php';
-
-    class CustomerUserManagementCtrl extends UserController{
-        public function __construct() {
-            parent::__construct();
-
-        }
-        
-        public function editCustomer($id = 0){
-            $customer = $this->setCustomerDetails($this->setUserObj($id));
-            
-            $data = [
-                'pageTitle' => 'Customer Editing Form',
-                'selectedUser' => $customer,
-                'action' => 'edit',
-            ];
-                        
-            $view = ['customerDetailInputFormView'];
-            $this->renderView($view, $data);
-
-            //alter the customer details
-        }
-        
-        public function customerUserManagement() {
-            $this->index();
-        }
-
-        public function index(){
-            $result = $this->processUsers();
-            
-            //set role for each data
-            $usersArray = $result['usersArray'];
-            $customersArray = $result['customersArray'];
-            
-            //set render data (set the user, customer, admin)
-            $data = $this->setRenderData('User Management Panel');
-            $data['usersArray'] = $usersArray;
-            $data['customersArray'] = $customersArray;
-            
-            $view = ['adminTopNavHeader','userManagementTopNav','customerUserManagementView'];
-            //display/render the user view
-                
-            $this->renderView($view,$data);
-            
-        }
-    }
-=======
-<?php
-
+    /*Author name: Chew Wei Seng*/
     require_once __DIR__ . '/../../Config/webConfig.php';
     $webConfig = new webConfig();
     $webConfig->restrictAccessForNonLoggedInAdmin();// only allow admin to access
@@ -104,4 +50,3 @@
             
         }
     }
->>>>>>> 6ea1a893419d37eb823b4036368cf06f04acc540
