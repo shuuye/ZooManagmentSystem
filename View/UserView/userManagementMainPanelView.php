5bbc1282c52e8b5647567d081d687cc30ead55a2
diedInside-fml <chewws-wp21@student.tarc.edu.my>
2024-09-22 15:16:25 +0800

    Update userManagementMainPanelView.php

--- View/UserView/userManagementMainPanelView.php
+++ View/UserView/userManagementMainPanelView.php
@@ -1,4 +1,5 @@
 <?php
+    /*Author Name: Chew Wei Seng*/
     require_once __DIR__ . '/../../Config/webConfig.php';
     $webConfig = new webConfig();
     $webConfig->restrictAccessForNonLoggedInAdmin();//only allow the logged in admin to access
